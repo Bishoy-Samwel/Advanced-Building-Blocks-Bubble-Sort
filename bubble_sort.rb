fb8ad426508e20402a934967b05d45c7b9a0f767
Bishoy-Samwel <bishoy.samwuel@gmail.com>
2021-02-10 17:29:36 +0200

    solve bubble sort problem

---
+++ bubble_sort.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+arr = %w[hey go hello d family]
+def bubble_sort_by(arr)
+  times = 0
+  run = arr.length - 1
+  run.times do
+    (0..arr.length - 2 - times).each do |i|
+      left = arr[i]
+      right = arr[i + 1]
+      next unless yield(left, right).positive?
+
+      temp_i = arr[i]
+      arr[i] = arr[i + 1]
+      arr[i + 1] = temp_i
+    end
+    times += 1
+  end
+  puts arr
+end
+bubble_sort_by(arr) do |left, right|
+  left.length - right.length
+end
